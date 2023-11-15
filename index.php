6c71d008fb7783242b23ef6dc402a1d2aed94849
Marlene <ugarte.marlene.942.@gmail.com>
2023-11-15 18:15:27 -0400

    intacias prueba

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estilo.css">
+    <title>Distribuidora Marina - Menú Principal</title>
+</head>
+<body>
+    <div class="menu-container">
+        <img src="img/logo.jpg" alt="Logo Distribuidora Marina" class="logo">
+        <h1>Bienvenido a Distribuidora</h1>
+        <div class="menu-options">
+            <div class="menu-option">
+                <img src="img/catalogo.jpg" alt="Catálogo de Productos">
+                <a href="#">Catálogo de Productos</a>
+            </div>
+            <div class="menu-option">
+                <img src="ofertas.png" alt="Ofertas Especiales">
+                <a href="#">Ofertas Especiales</a>
+            </div>
+            <!-- Agrega más opciones según sea necesario -->
+        </div>
+    </div>
+</body>
+</html>
